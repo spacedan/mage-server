05aa26dbc637b8a217b71d3f069fdf6b156375c3
William Newman <newmanw10@gmail.com>
2017-10-24 14:12:08 -0600

    Fix merge

--- models/observation.js
+++ models/observation.js
@@ -118,6 +118,10 @@ function transform(observation, ret, options) {
   }
 }
 
+ObservationIdSchema.set("toJSON", {
+  transform: transform
+});
+
 ObservationSchema.set('toJSON', {
   transform: transform
 });
